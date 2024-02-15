9526a4545789ee6c5bb6ff74247540300c22b796
cryptocowboye <144955974+cryptocowboye@users.noreply.github.com>
2024-02-15 01:47:27 -0500

    Add validation to form

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+
+
+const validatePassword = (password) => {
+    if (password.value === "" || password.value.length < 3) {
+        password.style.borderColor = 'red';
+    } else {
+        password.style.borderColor = '#d5d5d5'
+    }
+    return password
+}
+
+const confirmPassword = (password) => {
+    if (password.value !== passwordFields[0].value || password.value == '') {
+        password.style.borderColor = 'red'
+        if (para.classList !== "retypePassword") {
+            para.classList.add('retypePassword')
+            para.appendChild(paraContent)
+            passwordParent.appendChild(para)
+        }
+    } else {
+        password.style.borderColor = "#d5d5d5"
+    }
+    return password
+}
+
+const passwordFields = document.querySelectorAll('.error');
+const confirmBtn = document.querySelector('#submitBtn');
+
+const passwordParent = document.querySelector('.password-field');
+let para = document.createElement('p');
+let paraContent = document.createTextNode("* Passwords do not match")
+
+const confirmation = () => {
+    confirmBtn.addEventListener("click", function (event) {
+        validatePassword(passwordFields[0]);
+        confirmPassword(passwordFields[1]);
+        event.preventDefault()
+    });
+}
+
+confirmation()
